4ab606c02f4e6b71f9782ddf3e1ab193e01480c1
Mattavonhi <baibi11228136@126.com>
2021-05-15 13:26:01 +0800

    no commit message

---
+++ src/me/qinyang/drp/util/DbUtil.java
@@ -0,0 +1,31 @@
+package me.qinyang.drp.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.dom4j.DocumentException;
+
+import me.qinyang.drp.model.JdbcConfig;
+
+public class DbUtil {
+	public static Connection getConnection(){
+		Connection conn = null;
+		try {
+			JdbcConfig config = XmlConfigReader.getInstance().getConfig();
+			Class.forName(config.getDriverName());
+			conn = DriverManager.getConnection(config.getUrl(),config.getUserName(),config.getPassword());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return conn;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(DbUtil.getConnection());
+	}
+}
---
+++ src/me/qinyang/drp/util/XmlConfigReader.java
@@ -0,0 +1,49 @@
+package me.qinyang.drp.util;
+
+import java.io.InputStream;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import me.qinyang.drp.model.JdbcConfig;
+
+public class XmlConfigReader {
+	private static XmlConfigReader instance = null;
+	private JdbcConfig config = null;
+	private XmlConfigReader(){
+		
+		try {
+			SAXReader reader = new SAXReader();
+			InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("sys-conf.xml");
+			Document document;
+			document = reader.read(in);
+			Element driverName = (Element)document.selectObject("/config/db-info/driver-name");
+			Element url = (Element) document.selectObject("/config/db-info/url");
+			Element userName = (Element) document.selectObject("/config/db-info/user-name");
+			Element password = (Element) document.selectObject("/config/db-info/password");
+			config = new  JdbcConfig(driverName.getStringValue(),
+					url.getStringValue(),
+					userName.getStringValue(), 
+					password.getStringValue());
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	public static synchronized XmlConfigReader getInstance(){
+		if (instance == null) {
+			instance = new  XmlConfigReader();
+		}
+		return instance;
+	}
+	public JdbcConfig getConfig() {
+		return config;
+	}
+	
+	
+}
